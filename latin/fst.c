1dfa71838810332a92900361f0a09200dd67bcba
Harry Schmidt <me@goldibex.com>
2013-09-09 11:53:05 -0700

    fst.c is finished. wip -- will roll into the objc library

--- latin/fst.c
+++ latin/fst.c
@@ -5,23 +5,82 @@
 #include <stdlib.h>
 
 #define FST_LINE_MAX 256 // max line len in an FST source is 256 bytes
+#define FST_DEPTH_MAX 256 // max depth of nodes in FST
+#define FST_EPSILON "<>"
+#define FST_EPSILON_LEN 2
 
-typedef struct {
-  uint32_t    to;
-  uint8_t     last_in_list;
+typedef struct fst_node {
+  uint32_t    edge_list_offset;
+  uint16_t    edge_list_len;
   uint8_t     terminal;
-  const char* in;
-  const char* out;
+} fst_node_t;
+
+typedef struct fst_edge {
+  uint32_t    to;
+  uint16_t    in;
+  uint16_t    out;
 } fst_edge_t;
 
-typedef struct {
+typedef struct fst {
+  fst_node_t* nodes;
   fst_edge_t* edges;
-  char*       symbols;
+  char**      symbols;
+  int         node_count;
+  int         edge_count;
+  int         symbol_count;
 } fst_t;
+
+typedef struct fst_stack {
+  const fst_node_t* nodes[FST_DEPTH_MAX];
+  uint16_t edge_list_pos[FST_DEPTH_MAX];
+  const char* in_tapes[FST_DEPTH_MAX];
+  int depth;
+} fst_stack_t;
+
+typedef struct fst_iterator {
+  fst_t* fst;
+  fst_stack_t stack;
+} fst_iterator_t;
+
+void
+fst_stack_push(fst_stack_t* stack, const fst_node_t* node, uint16_t pos, const char* in_tape) {
+  if (stack->depth > FST_DEPTH_MAX) {
+    return;
+  }
+  stack->depth++;
+  stack->nodes[stack->depth] = node;
+  stack->edge_list_pos[stack->depth] = pos;
+  stack->in_tapes[stack->depth] = in_tape;
+}
+
+int
+fst_stack_pop(fst_stack_t* stack, const fst_node_t** node, uint16_t *pos, const char** in_tape) {
+  while (stack->depth > 0) {
+    int next_pos = stack->edge_list_pos[stack->depth]++;
+    if (next_pos < stack->nodes[stack->depth]->edge_list_len) {
+      *pos = next_pos;
+      *node = stack->nodes[stack->depth];
+      *in_tape = stack->in_tapes[stack->depth];
+      return 1;
+    }
+    stack->depth--;
+  }
+  *pos = 0;
+  *node = NULL;
+  *in_tape = NULL;
+  return 0;
+}
 
 void
 fst_destroy(fst_t* fst) {
+  free(fst->nodes);
   free(fst->edges);
+
+  int i;
+  for (i = 0; i < fst->symbol_count; i++) {
+    free(fst->symbols[i]);
+  }
+
   free(fst->symbols);
 }
 
@@ -51,12 +110,10 @@ fst_load_att(FILE* ats, FILE* ate, fst_t* fst) {
   rewind(ats);
 
   // find out how much memory we need for the edge tables
-  int edge_count = 0,
-      terminal_node_count = 0;
+  int edge_count = 0;
   while (fgets(fst_line, FST_LINE_MAX, ate)) {
-    switch (sscanf(fst_line, "%d %d %d %d", &from, &to, &in, &out)) {
-      case 1: // terminal node indicator
-        terminal_node_count++;
+    switch (sscanf(fst_line, "%d %d %d %d", &from, &to, &out, &in)) {
+      case 1: // terminal node
         break;
       case 4: // edge
         edge_count++;
@@ -75,16 +132,16 @@ fst_load_att(FILE* ats, FILE* ate, fst_t* fst) {
   // allocate the memory for the tables
   int node_count = from + 1;
 
-  fst_edge_t* edges = malloc(edge_count * sizeof(*edges));
-  char* symbols = malloc(symbol_size * sizeof(*symbols));
+  fst_node_t* nodes = calloc(node_count, sizeof(*nodes));
+  fst_edge_t* edges = calloc(edge_count, sizeof(*edges));
+  char** symbols = calloc(symbol_count, sizeof(*symbols));
 
+  fst->nodes = nodes;
   fst->edges = edges;
   fst->symbols = symbols;
-
-  char** symbol_lookup = malloc(symbol_count * sizeof(*symbol_lookup));
-  uint32_t* node_lookup = malloc(node_count * sizeof(*node_lookup));
-  uint32_t* terminal_nodes = calloc(node_count, sizeof(*terminal_nodes));
-  node_lookup[0] = 0;
+  fst->node_count = node_count;
+  fst->edge_count = edge_count;
+  fst->symbol_count = symbol_count;
 
   // read the symbol table
   i = 0;
@@ -92,50 +149,133 @@ fst_load_att(FILE* ats, FILE* ate, fst_t* fst) {
     int symbol_len = strlen(fst_line) - 1;
     fst_line[symbol_len] = '\0'; // chomp
 
-    strcpy(symbols, fst_line);
-    symbol_lookup[i++] = symbols;
-    symbols += symbol_len + 1;
+    symbols[i++] = strdup(fst_line);
   }
 
   // read the edge table  
   i = 0;
-  int terminal_node_id = 0,
-      prev_from = 0;
+  int prev_from = 0,
+      j = 0;
   while (fgets(fst_line, FST_LINE_MAX, ate)) {
-    switch (sscanf(fst_line, "%d %d %d %d", &from, &to, &in, &out)) {
+    switch (sscanf(fst_line, "%d %d %d %d", &from, &to, &out, &in)) {
       case 1: // terminal node indicator
-        terminal_nodes[from] = 1;
+        nodes[from].terminal = 1;
         break;
       case 4: // edge
         if (from != prev_from) {
-          node_lookup[from] = i;
+          if (i > 0) {
+            nodes[prev_from].edge_list_len = j;
+          }
+          nodes[from].edge_list_offset = i;
           prev_from = from;
+          j = 0;
         }
-        edges[i] = (fst_edge_t){to, 0, 0, symbol_lookup[in], symbol_lookup[out]};
+        edges[i++] = (fst_edge_t){to, in, out};
+        j++;
         break;
     }
-    i++;
   }
-
-  // one last loop, to set the edges' 'to' pointer properly
-  for (i = 0; i < edge_count; i++) {
-    if (terminal_nodes[edges[i].to]) {
-      edges[i].terminal = 1;
-    }
-    edges[i].to = node_lookup[edges[i].to];
-  }
-
-  /*
- * // one last loop, to set the edges' "to" pointer properly
-  while (i > 0) {
-    edges[i].to = node_lookup[j] - node_lookup;
-    i--;
-  }
-*/
-  free(terminal_nodes);
-  free(node_lookup);
-  free(symbol_lookup);
+  nodes[prev_from].edge_list_len = j;
   return 0;
+}
+
+void
+fst_print(fst_t* fst) {
+  int i, j;
+  for (i = 0; i < fst->node_count; i++) {
+    for (j = 0; j < fst->nodes[i].edge_list_len; j++) {
+      fst_edge_t* edge = fst->edges + fst->nodes[i].edge_list_offset + j;
+      printf("%d %d %s %s\n", i, edge->to, fst->symbols[edge->out], fst->symbols[edge->in]);
+    }
+    if (fst->nodes[i].terminal) {
+      printf("%d\n", i);
+    }
+  }
+}
+
+void
+fst_iterator_reset(fst_t* fst, fst_iterator_t* iter, const char* in_tape) {
+  iter->fst = fst;
+  
+  iter->stack.nodes[0] = NULL;
+  iter->stack.edge_list_pos[0] = 0;
+  iter->stack.in_tapes[0] = NULL;
+
+  iter->stack.depth = 1;
+  iter->stack.nodes[1] = fst->nodes;
+  iter->stack.edge_list_pos[1] = 0;
+  iter->stack.in_tapes[1] = in_tape;
+}
+
+inline int
+fst_epsilon(const char* symbol) {
+  return (strlen(symbol) == FST_EPSILON_LEN &&
+      strcmp(symbol, FST_EPSILON) == 0);
+}
+
+const char*
+fst_match(const char* in_tape, const char* symbol) {
+  size_t symbol_len = strlen(symbol),
+         in_tape_len = strlen(in_tape);
+  if (fst_epsilon(symbol)) {
+      return in_tape;
+  }
+  if (symbol_len > in_tape_len) {
+    return NULL;
+  }
+  if (strncmp(symbol, in_tape, symbol_len) == 0) {
+    return in_tape + symbol_len;
+  }
+  return NULL;
+}
+
+int
+fst_iterate(fst_iterator_t* iter) {
+  const char
+    *in_tape,
+    *new_in_tape,
+    *symbol;
+
+  fst_t* fst = iter->fst;
+  fst_edge_t* edge;
+  const fst_node_t
+    *node,
+    *node_to;
+  uint16_t pos;
+
+  while (fst_stack_pop(&iter->stack, &node, &pos, &in_tape)) {
+    edge = fst->edges + node->edge_list_offset + pos;
+    symbol = fst->symbols[edge->in];
+    if (new_in_tape = fst_match(in_tape, symbol)) {
+      node_to = fst->nodes + edge->to;
+      fst_stack_push(&iter->stack, node_to, 0, new_in_tape);
+      if (strlen(new_in_tape) == 0 && node_to->terminal) {
+        return 1;
+      }
+    }
+  }
+  return 0;
+}
+
+void
+fst_iterator_out(fst_iterator_t* iter, char* in, int n) {
+  fst_t* fst = iter->fst;
+  fst_stack_t* stack = &iter->stack;
+  fst_edge_t* edge;
+  const char* symbol;
+
+  // wipe out any existing string in the input buffer
+  *in = '\0';
+
+  int i;
+  for (i = 1; i < stack->depth; i++) {
+    edge = fst->edges + stack->nodes[i]->edge_list_offset + stack->edge_list_pos[i] - 1;
+    symbol = fst->symbols[edge->out];
+    if (!fst_epsilon(symbol)) {
+      strncat(in, symbol, n);
+      n -= strlen(symbol);
+    }
+  }
 }
 
 int
@@ -161,6 +301,24 @@ main(int argc, char *argv[]) {
   fst_t fst;
   if (fst_load_att(ats_file, ate_file, &fst) != 0) {
     printf("fatal error: aborting\n");
+  }
+
+  fst_iterator_t iter;
+  char in[256], out[256];
+  memset(in, 0, 256);
+  memset(out, 0, 256);
+  while (1) {
+    printf("Inflected form: ");
+    fgets(in, 256, stdin);
+    in[strlen(in) - 1] = '\0'; // chomp
+    if (strlen(in) == 0) {
+      return 0;
+    }
+    fst_iterator_reset(&fst, &iter, in);
+    while (fst_iterate(&iter)) {
+      fst_iterator_out(&iter, out, 256);
+      printf("match: %s\n", out);
+    }
   }
 
   fst_destroy(&fst);
